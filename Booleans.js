d16f17e1aafe7759e829161acd5e9ead46949598
Isidora Castillo <isidoradanae504@gmail.com>
2023-07-03 21:54:12 -0400

    Arreglando archivo booleans.js

--- Booleans.js
+++ Booleans.js
@@ -1,9 +1,6 @@
 // Crea 2 variables con los 2 valores booleanos que existen.
 
 const bolean1 = true
-<<<<<<< HEAD
-const bolean2 = false
-=======
 const bolean2 = true
 
 // y el valor false en la variable?, eran 2 variables con los 2 valores que existen.
